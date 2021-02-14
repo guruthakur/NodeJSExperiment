4298c386005f8088b689be907f2594dcc0f180a7
gurunath thakur <guruthakur16@gmail.com>
2021-02-14 23:16:36 +0530

    In-build modules
    How to use the in build modules and how to use the event module

--- app.js
+++ app.js
@@ -1,4 +1,40 @@
-var stuff = require('./stuff');
-console.log(stuff.counter(['gurunath', 'bhushan']));
-console.log(stuff.adder(5,6));
-console.log(stuff.adder(5,stuff.pi));
+var events = require('events');
+const { hrtime } = require('process');
+var util = require('util');
+
+
+var Person = function (name) {
+    this.name = name;
+};
+
+util.inherits(Person, events.EventEmitter);
+var gurunath = new Person('gurunath');
+var bhushan = new Person('bhushan');
+var james = new Person('james');
+var HBK = new Person('HBK');
+
+
+var people = [gurunath, bhushan, james, HBK];
+
+people.forEach(function (person) {
+    person.on('speak', function (msg) {
+        console.log(person.name + ' said: ' + msg);
+    })
+})
+james.emit('speak', 'hey dudes');
+HBK.emit('speak', 'I want a curry');
+
+
+/*
+# Case 1 Sample
+// Sample how the emitter work and what are syntax
+var myEmitter = new events.EventEmitter();
+
+
+myEmitter.on('someEvent',function(msg){
+    console.log(msg);
+});
+
+myEmitter.emit('someEvent','The event was emitted');
+
+*/

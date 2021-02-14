e0395f05ad2aa5c0bc1ee89d04f0f644c700c5e3
gurunath thakur <guruthakur16@gmail.com>
2021-02-14 22:05:44 +0530

    Create App.js
    Hello world example with some basic console.log function and global functions

---
+++ App.js
@@ -0,0 +1,18 @@
+var time =0;
+
+/*
+
+// Sample 2 
+var timer = setInterval(function(){
+    time += 2;
+
+    console.log( time + " seconds have passed");
+    if(time > 5 ){
+        clearInterval(timer);
+    }
+},3000)
+*/
+
+// Sample 3 
+console.log(__dirname);  // return folder path
+console.log(__filename);// return file full path

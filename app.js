624eee9a8fb8eafe1fe7523f6cb021d28fa7af56
gurunath thakur <guruthakur16@gmail.com>
2021-02-14 23:02:54 +0530

    Module Patterns
    Different way to handle the patterns and how the calling is done.

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-var counter = require('./count');
-console.log(counter(['gurunath', 'bhushan']));
+var stuff = require('./stuff');
+console.log(stuff.counter(['gurunath', 'bhushan']));
+console.log(stuff.adder(5,6));
+console.log(stuff.adder(5,stuff.pi));
